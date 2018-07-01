162f7a7bdbbaefa839af01a2dab6ff5510d5f2aa
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-07-01 14:26:37 +0200

    Fix class name.

--- lib/generators/ecm/sliders/backend/install/install_generator.rb
+++ lib/generators/ecm/sliders/backend/install/install_generator.rb
@@ -1,16 +1,20 @@
-module Ecm_Sliders_Backend
-  module Generators
-    class InstallGenerator < Rails::Generators::Base
-      desc 'Generates the initializer'
+module Ecm
+  module Sliders
+    module Backend
+      module Generators
+        class InstallGenerator < Rails::Generators::Base
+          desc 'Generates the initializer'
 
-      source_root File.expand_path('../templates', __FILE__)
+          source_root File.expand_path('../templates', __FILE__)
 
-      def generate_initializer
-        template 'initializer.rb', 'config/initializers/ecm_sliders_backend.rb'
-      end
+          def generate_initializer
+            template 'initializer.rb', 'config/initializers/ecm_sliders_backend.rb'
+          end
 
-      def generate_routes
-        route File.read(File.join(File.expand_path('../templates', __FILE__), 'routes.source'))
+          def generate_routes
+            route File.read(File.join(File.expand_path('../templates', __FILE__), 'routes.source'))
+          end
+        end
       end
     end
   end
